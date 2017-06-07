d50db1fa607af376d220cebeb914d0953192dd41
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2017-06-07 23:22:53 +0300

    Project structure, scripts to separate files

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Practicing HTML5, JavaScript, jQuery, CSS etc.
 
 ### SoftwareEngineerLearns
 Using the Kindle version of 'A Software Engineer Learns HTML5, JavaScript and jQuery: A guide to standards-based web applications' by Dane Cameron to get familiar with
-JavaScript and jQuery. Trying to use SCSS for the styles.
+JavaScript and jQuery by building a single-page web application. Trying to use SCSS for the styles.
 
---
+++ softwareEngineerLearns/scripts/tasks-controller.js
@@ -0,0 +1,73 @@
+tasksController = function() {
+	let taskPage;
+	let initialized = false;
+
+	// "When rending an underscore template, we want top-level
+  // variables to be referenced as part of an object. For
+  // technical reasons (scope-chain search), this speeds up
+  // rendering; however, more importantly, this also allows our
+  // templates to look / feel more like our server-side
+  // templates that use the rc (Request Context / Colletion) in
+  // order to render their markup."
+	 _.templateSettings.variable = "rc";
+
+	return {
+		// Any initialization tasks that need to occur when tasks.html loads
+		init: function(page) {
+			if (!initialized) {
+				taskPage = page;  // This is kind of private
+
+				// Append star * to required fields
+				$(taskPage)
+						.find('[required]')
+						.prev('label')
+						.append('<span>*</span>')
+						.children('span')
+						.addClass('required');
+
+				// On button-click, hide taskCreation section
+				$(taskPage)
+						.find('#btnAddTask')
+						.click(evt => { 
+							evt.preventDefault();
+							$('#taskCreation').removeClass('not');
+						});
+
+				// Highlight rows when clicked
+				$(taskPage)
+						.find('#tblTasks tbody')
+						.on('click', '.taskRow', evt => {
+								$(evt.target)
+								.closest('td')
+								.siblings()
+								.addBack()
+								.toggleClass('rowHighlight');
+							});
+
+				// Delete a row upon delete button click
+				$(taskPage)
+						.find('#tblTasks tbody')
+						.on('click', '.deleteRow', evt => {
+								evt.preventDefault();
+								$(evt.target).parents('tr').remove();
+							});
+
+				// Save task button appends a new row to the table with the task info
+				// with the help of an _ template
+				$(taskPage)
+						.find('#saveTask')
+						.click(evt => {
+								evt.preventDefault();
+								let task = $(taskPage).find('form').toObject();
+								let rowTemplate = $(taskPage).find('#addRow').html(); // TODO Get right template
+								$(taskPage).find('#tblTasks tbody')
+										.append(_.template(rowTemplate)(task));
+							});
+			
+			initialized = true;
+			}
+		}
+	} 
+}(); 	/* Note the last parenthesis! Instantly executed, 
+			 therefore tasksController is the return value, not the function.
+			 This way no other code can construct another tasksController */
