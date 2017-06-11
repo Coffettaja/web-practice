223a71b7d2f530cab28c824910e63b7683f3533d
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2017-06-11 16:06:03 +0300

    Indexed db skeleton

---
+++ softwareEngineerLearns/scripts/tasks-indexeddb.js
@@ -0,0 +1,31 @@
+storageEngine = function() {
+	return {
+		init: function(successCallback, errorCallback) {
+
+		},
+
+		initObjectStore: function(type, successCallback, errorCallback) {
+
+		},
+
+		save: function(type, obj, successCallback, errorCallback) {
+
+		},
+
+		findAll: function(type, successCallback, errorCallback) {
+
+		},
+
+		delete: function(type, id, successCallback, errorCallback) {
+
+		},
+
+		findByProperty: function(type, propertyName, propertyValue, successCallback, errorCallback) {
+
+		},
+
+		findById: function(type, id, successCallback, errorCallback) {
+
+		}
+	}
+}();
