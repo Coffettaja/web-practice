caf8a259d3fd31c7321e72b67b8bb2215a20ea8d
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2017-06-11 16:14:49 +0300

    IndexedDB init

--- softwareEngineerLearns/scripts/tasks-indexeddb.js
+++ softwareEngineerLearns/scripts/tasks-indexeddb.js
@@ -1,7 +1,26 @@
 storageEngine = function() {
+	let database;
+	let objectStores;
+
 	return {
 		init: function(successCallback, errorCallback) {
+				if (window.indexedDB) {
+					// Request to open the database. Database is named based on the domain name.
+					// E.g. www.testing.com ==> www.testing.comDB
+					let request = indexedDB.open(window.location.hostname+'DB', 1);
 
+					request.onsuccess = function(event) {
+						database = request.result;
+						successCallback(null);
+					}
+
+					request.onerror = function(event) {
+						errorCallback("storage_not_initialized", "It is not possible to initialize the storage.");
+					}
+				}
+				else {
+					errorCallback("storage_api_not_supported", "The web storage API is not supported.");
+				}
 		},
 
 		initObjectStore: function(type, successCallback, errorCallback) {
