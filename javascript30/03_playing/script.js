f001c905bd9eef14e226bbf8542c752f887a2a23
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2017-07-22 17:01:18 +0300

    tutorial part complete

--- javascript30/03_playing/script.js
+++ javascript30/03_playing/script.js
@@ -1,5 +1,12 @@
 "use strict";
 
 $(function () {
+	const $inputs = $('.controls input')
 
+	function handleUpdate() {
+		const suffix = this.dataset.sizing || ''
+		$(':root').css(`--${this.name}`, this.value + suffix)
+	}
+
+	$inputs.each((i, element) => element.oninput = handleUpdate)
 })
