fe8395cd83d6c288f77e236bf41dcd72e4977f09
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2017-07-24 22:31:57 +0300

    Got the toggling mostly working

--- javascript30/05_flex_gallery/script.js
+++ javascript30/05_flex_gallery/script.js
@@ -1,32 +1,36 @@
 $(function() {
-	let $lastClicked = $('.panel1')
-	let $lastClickedTemp = $('.panel1')
-	let $nowClicked = $('.panel1')
+	let $lastClicked = $('.panels')
+	let $nowClicked = $('.panels')
+	let clicked = false // hack to avoid problems caused by transitionend event happening twice in a row
 
+  // Toggles the 'open' class for the panels and sets the $nowClicked to the panel clicked
 	function toggleOpen(e) {
-		// console.log(e)
+		$(this).toggleClass('open')
+		if (!$(this).is($lastClicked)) {
+			$lastClicked.removeClass('open')
+		}
+
 		$nowClicked = $(this)
-		let theClass = e.data.theClass
-		if (!!$lastClicked.is($(this))) {
-			$lastClicked.removeClass(theClass)
-			// console.log(`${theClass} removed from `)
-			// console.log($lastClicked)
-		}
-			$lastClicked.toggleClass(theClass)
-
-		// $(this).toggleClass('open open-active')
-		console.log($(this))
-		$(this).addClass(theClass)
-		$lastClickedTemp = $(this)
+		clicked = true
 	}
 
+	// Toggles the 'open-active' class for the panels and sets 
+	// the $lastClicked to the last clicked panel
 	function toggleActive(e) {
-		if (e.originalEvent.propertyName.includes('flex')) {
-			toggleOpen.apply($nowClicked, [e])
+		if (e.originalEvent.propertyName.includes('flex') && clicked) {
+			$nowClicked.toggleClass('open-active')
+			if (!$lastClicked.is($nowClicked)) {
+				$lastClicked.removeClass('open-active')
+				$nowClicked.addClass('open-active')
+			}
+			$lastClicked = $nowClicked
+			clicked = false
 		}
-		$lastClicked = $lastClickedTemp
 	}
 
-	$('.panel').on('click', {theClass: 'open'}, toggleOpen)
-	$('.panel').on('transitionend webkitTransitionEnd oTransitionEnd', {theClass: 'open-active'}, toggleActive)
+	// This still breaks when opening multiple panels at the same time....
+	// Giving up for now
+	// TODO fix later
+	$('.panel').on('click', toggleOpen)
+	$nowClicked.on('transitionend webkitTransitionEnd oTransitionEnd', toggleActive)
 })
