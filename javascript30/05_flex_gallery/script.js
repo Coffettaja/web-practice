520278bfa650294392e7a6ce1a131469af7d25d2
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2017-07-25 21:01:32 +0300

    Added some precaution against spam clicking

--- javascript30/05_flex_gallery/script.js
+++ javascript30/05_flex_gallery/script.js
@@ -1,6 +1,7 @@
 $(function() {
 	let $lastClicked = $('.panels')
 	let $nowClicked = $('.panels')
+	let $panels = $('.panels')
 	let clicked = false // hack to avoid problems caused by transitionend event happening twice in a row
 
   // Toggles the 'open' class for the panels and sets the $nowClicked to the panel clicked
@@ -10,13 +11,18 @@ $(function() {
 			$lastClicked.removeClass('open')
 		}
 
+		// in case the user tries to interact with multiple panels very quickly
+		if (!$nowClicked.is($(this))) {
+			$nowClicked.removeClass('open')
+		}
+
 		$nowClicked = $(this)
 		clicked = true
 	}
 
 	// Toggles the 'open-active' class for the panels and sets 
 	// the $lastClicked to the last clicked panel
-	function toggleActive(e) {
+	function toggleActive(e) {	
 		if (e.originalEvent.propertyName.includes('flex') && clicked) {
 			$nowClicked.toggleClass('open-active')
 			if (!$lastClicked.is($nowClicked)) {
@@ -28,8 +34,9 @@ $(function() {
 		}
 	}
 
-	// This still breaks when opening multiple panels at the same time....
+	// This still breaks when click spamming panels....
 	// Giving up for now
+	// The smart way (or just not dumb way) would be to just set timer when panel clicked
 	// TODO fix later
 	$('.panel').on('click', toggleOpen)
 	$nowClicked.on('transitionend webkitTransitionEnd oTransitionEnd', toggleActive)
