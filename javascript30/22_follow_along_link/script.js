a6fe24409bd4277e0768881fc2a7a692086d2b10
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-10-22 19:41:13 +0300

    tutorial part complete

--- javascript30/22_follow_along_link/script.js
+++ javascript30/22_follow_along_link/script.js
@@ -1,1 +1,21 @@
 'use strict';
+const triggers = document.querySelectorAll('.menu__link')
+const highlight = document.createElement('span')
+highlight.classList.add('highlight')
+document.body.append(highlight)
+
+function highlightLink() {
+  // gets the smallest rectangle which contains the entire element, as a DOMRect object
+  const linkCoords = this.getBoundingClientRect()
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  }
+  highlight.style.width = `${coords.width}px`
+  highlight.style.height = `${coords.height}px`
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink))
