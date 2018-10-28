7cebbef7108af7788e0970baf810e9595e5f7010
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-10-28 20:25:32 +0200

    new Table component

---
+++ reviewing_react/react_refresher/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Table from './Table'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Table></Table>
+      </div>
+    )
+  }
+}
+
+export default App
